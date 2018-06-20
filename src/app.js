21275d7d61c69939c98ae6e476fac40107f41a00
clx1608@qq.com <hbdennischen@outlook.com>
2018-06-20 23:25:21 +0800

    use vuetify to create application layout

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
+import Vuetify from 'vuetify'
+import 'babel-polyfill'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../assets/app.styl'
 
-Vue.use(BootstrapVue);
+Vue.use(BootstrapVue)
+Vue.use(Vuetify)
+
 new Vue({
 	el: '#app',
 	render: h => h(App)
