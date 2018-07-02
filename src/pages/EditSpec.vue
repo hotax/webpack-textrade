0336632408182ec43d53311b45bf2700345659a4
clx1608@qq.com <hbdennischen@outlook.com>
2018-07-02 11:39:31 +0800

    Spec form

--- src/pages/EditSpec.vue
+++ src/pages/EditSpec.vue
@@ -1,5 +1,121 @@
 <template>
-    <v-layout row>
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-8">
+            <form>
+                <div class="form-row mt-2 mb-4 border-bottom align-items-end row justify-content-between">
+                    <h2>产品规格</h2>
+                    <span>标识：{{spec.id}}</span>
+                    <span>日期：{{spec.modifiedDate}}</span>
+                </div>
+
+                <div class="form-row">
+                    <div class="form-group col-12 col-sm-3">
+                        <label for="code">编号：</label>
+                        <input type="text" class="form-control" id="code" v-model='spec.code'>
+                    </div>
+                    <div class="form-group col-12 col-sm-8 ml-sm-auto">
+                        <label for="name">名称：</label>
+                        <input type="text" class="form-control" id="name" v-model="spec.name">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="desc">描述</label>
+                    <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                </div>
+
+                <div class="row mb-2">
+                    <div class="col col-auto">
+                        <span>坯布:</span>
+                    </div>
+                    <div class="col border">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <div class="row align-items-end">
+                                            <label>纱支：</label>
+                                        </div>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="desc">密度：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                                <div class="col col-4">
+                                    <div class="form-group">
+                                        <label for="desc">门幅：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="desc">克重：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col col-auto">
+                        <span>成品:</span>
+                    </div>
+                    <div class="col border">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="desc">纱支：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="desc">密度：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="desc">密度：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="form-group">
+                                        <label for="desc">门幅：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="desc">克重：</label>
+                                        <input type="text" class="form-control" id="desc" v-model="spec.desc">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <button type="确认" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- <v-layout row>
         <v-flex xs10 offset-xs1 md8 offset-md2>
             <form>
                 <v-text-field v-model="name" :error-messages="nameErrors" :counter="10" label="Name" required @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
@@ -12,7 +128,8 @@
                 <v-btn @click="clear">clear</v-btn>
             </form>
         </v-flex>
-    </v-layout>
+    </v-layout> -->
+
 </template>
 
 <script>
@@ -46,16 +163,74 @@
         },
 
         data: () => ({
-            name: '',
-            email: '',
-            select: null,
-            items: [
-                'Item 1',
-                'Item 2',
-                'Item 3',
-                'Item 4'
-            ],
-            checkbox: false
+            spec: {
+                id: '1234556677899',
+                code: '00001',
+                name: '6850 2/3斜（左右捻）',
+                desc: 'desc of 6850 2/3斜（左右捻）',
+                constructure: '2/3斜',
+                grey: { //'坯布' 
+                    yarn: { //纱织 
+                        warp: {
+                            value: [6850],
+                            unit: 'g'
+                        },
+                        weft: {
+                            value: [6850],
+                            unit: 'g'
+                        }
+                    },
+                    dnsty: { //密度 
+                        warp: {
+                            value: [6850],
+                            unit: 'g'
+                        },
+                        weft: {
+                            value: [6850],
+                            unit: 'g'
+                        }
+                    },
+                    width: "'178(70')",
+                    GSM: 150
+                },
+                product: {
+                    yarn: {
+                        warp: {
+                            value: [6850],
+                            unit: 'g'
+                        },
+                        weft: {
+                            value: [6850],
+                            unit: 'g'
+                        }
+                    },
+                    dnstyBW: {
+                        warp: {
+                            value: [6850],
+                            unit: 'g'
+                        },
+                        weft: {
+                            value: [6850],
+                            unit: 'g'
+                        }
+                    },
+                    dnstyAW: {
+                        warp: {
+                            value: [6850],
+                            unit: 'g'
+                        },
+                        weft: {
+                            value: [6850],
+                            unit: 'g'
+                        }
+                    },
+                    width: "325(128')",
+                    GSM: 250
+                },
+                createDate: new Date(2018, 6,
+                    27),
+                modifiedDate: new Date(2018, 6, 27)
+            }
         }),
 
         computed: {
